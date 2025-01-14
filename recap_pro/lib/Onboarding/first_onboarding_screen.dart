7eb14a908911237c1f7db7139b665a5612c91dbf
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-14 14:00:06 +0200

    Edited: First onboarding added title

--- recap_pro/lib/Onboarding/first_onboarding_screen.dart
+++ recap_pro/lib/Onboarding/first_onboarding_screen.dart
@@ -6,15 +6,28 @@ class Onboarding1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity, // Full width of the screen
-        constraints: const BoxConstraints(
-          maxHeight: 500.0, // Set a maximum height
-        ),
-        child: Image.asset(
-          "./assets/Onboarding1.png", // Corrected image path
-          fit: BoxFit
-              .cover, // Optional: Adjusts how the image fits within the constraints
+      body:   Center(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity, // Full width of the screen
+              constraints: const BoxConstraints(
+                maxHeight: 500.0, // Set a maximum height
+              ),
+              child: Image.asset(
+                "./assets/Onboarding1.png", // Corrected image path
+                fit: BoxFit
+                    .cover, // Optional: Adjusts how the image fits within the constraints
+              ),
+            ),
+                      const Text(
+            "GAP FILLING",
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+          ],
         ),
       ),
     );
