a7c94d91833064615237190ba75147574974776c
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-14 14:05:24 +0200

    Edited: Third onboarding added content

--- recap_pro/lib/Onboarding/third_onboarding_screen.dart
+++ recap_pro/lib/Onboarding/third_onboarding_screen.dart
@@ -5,16 +5,40 @@ class Onboarding3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity, // Full width of the screen
-        constraints: const BoxConstraints(
-          maxHeight: 500.0, // Set a maximum height
-        ),
-        child: Image.asset(
-          "./assets/Onboarding3.png", // Corrected image path
-          fit: BoxFit
-              .cover, // Optional: Adjusts how the image fits within the constraints
+ return Scaffold(
+      body:   Center(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity, // Full width of the screen
+              constraints: const BoxConstraints(
+                maxHeight: 500.0, // Set a maximum height
+              ),
+              child: Image.asset(
+                "./assets/Onboarding3.png", // Corrected image path
+                fit: BoxFit
+                    .cover, // Optional: Adjusts how the image fits within the constraints
+              ),
+            ),
+                      const Text(
+            "VIDEO SEGMENTATION",
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+                    const SizedBox(
+            width: 350.0,
+            child: Text(
+              "Automatically organize recordings into clear, topic-based sections with smart video segmentation.",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              textAlign: TextAlign.center,
+            ),
+          )
+          ],
         ),
       ),
     );
