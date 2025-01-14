e681f184f6be61c98ab64dbda585c5a10dc22ec7
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-14 14:04:08 +0200

    Edited: Second onboarding added content

--- recap_pro/lib/Onboarding/second_onboarding_screen.dart
+++ recap_pro/lib/Onboarding/second_onboarding_screen.dart
@@ -6,17 +6,41 @@ class Onboarding2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity, // Full width of the screen
-        constraints: const BoxConstraints(
-          maxHeight: 500.0, // Set a maximum height
-        ),
-        child: Image.asset(
-          "./assets/Onboarding2.png", // Corrected image path
-          fit: BoxFit
-              .cover, // Optional: Adjusts how the image fits within the constraints
+      body:   Center(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity, // Full width of the screen
+              constraints: const BoxConstraints(
+                maxHeight: 500.0, // Set a maximum height
+              ),
+              child: Image.asset(
+                "./assets/Onboarding2.png", // Corrected image path
+                fit: BoxFit
+                    .cover, // Optional: Adjusts how the image fits within the constraints
+              ),
+            ),
+                      const Text(
+            "CONTENT EXTRACTOR",
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+                    const SizedBox(
+            width: 350.0,
+            child: Text(
+              "Effortlessly extract and organize video content with precision and speed.",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              textAlign: TextAlign.center,
+            ),
+          )
+          ],
         ),
       ),
     );
   }
-}
+  }
