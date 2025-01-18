666df8a5e8014c6015031d3694bb7bbcc00b12b7
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 12:38:25 +0200

    Edited: Design added title design <> dart

--- recap_pro/lib/utils/design.dart
+++ recap_pro/lib/utils/design.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-const TextStyle buttonTextStyle = TextStyle(color: Colors.black);
+const TextStyle title =TextStyle(fontSize: 24.0, color: Color.fromARGB(255, 0, 0, 0));
 
+const TextStyle buttonTextStyle = TextStyle(color: Colors.black);
 
 const ExpandingDotsEffect indicatorEffect = ExpandingDotsEffect(
   activeDotColor: Color(0xFF61DBFB),
