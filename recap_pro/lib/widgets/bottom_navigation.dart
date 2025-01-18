6cb7df3b3291b997e85dc20f9c24cbc89e936633
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 16:34:34 +0200

    Edited: Bottom navigation added page selected

--- recap_pro/lib/widgets/bottom_navigation.dart
+++ recap_pro/lib/widgets/bottom_navigation.dart
@@ -55,7 +55,7 @@ class BottomNavigation extends StatelessWidget {
           ),
         ],
         onDestinationSelected: onPageSelected,
-
+        selectedIndex: currentPage,
       ),
     );
   }
