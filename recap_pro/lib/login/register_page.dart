1266d4de1f7834c62cce2c8c22af5405a99aab00
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 15:01:02 +0200

    Edited: Register page added lname field

--- recap_pro/lib/login/register_page.dart
+++ recap_pro/lib/login/register_page.dart
@@ -70,6 +70,22 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
               ),
             ),
+            SizedBox(height: 5.0,),
+            SizedBox(
+              width: double.infinity,
+              height: 50.0,
+              child: Padding(
+                padding: textFieldPadding,
+                child: TextField(
+                    controller: _lnameController,
+                    style: const TextStyle(fontSize: 18.0),
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Last Name")),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
           ],
         ),
       ),
