4d1130cd18b93cbeee41f08e08f0d53ccb9219b7
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 15:04:09 +0200

    Edited: Register page added password field

--- recap_pro/lib/login/register_page.dart
+++ recap_pro/lib/login/register_page.dart
@@ -123,6 +123,21 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
             ),
             SizedBox(height: 5.0,),
+                            SizedBox(
+              width: double.infinity,
+              height: 50.0,
+              child: Padding(
+                padding: textFieldPadding,
+                child: TextField(
+                    controller: _passwordController,
+                    style: const TextStyle(fontSize: 18.0),
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Password")),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
           ],
         ),
       ),
