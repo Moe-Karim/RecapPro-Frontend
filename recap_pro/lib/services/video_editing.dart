f21abc8bf799dda28eb6b9b471e257ecd1cbe1b0
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-19 00:18:05 +0200

    Edited: Video editing imported open_file package

--- recap_pro/lib/services/video_editing.dart
+++ recap_pro/lib/services/video_editing.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
