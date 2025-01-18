a8598bfc3aa619f7f48fab2064cd22087b0790d6
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 12:25:41 +0200

    Added: Indicator effects <> dart

--- recap_pro/lib/utils/design.dart
+++ recap_pro/lib/utils/design.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
 const TextStyle buttonStyle = TextStyle(color: Colors.black);
+
+
+const ExpandingDotsEffect indicatorEffect = ExpandingDotsEffect(
+  activeDotColor: Color(0xFF61DBFB),
+  dotColor: Color(0xFFBDBDBD),
+  dotHeight: 7.0,
+  dotWidth: 7.0,
+  spacing: 5.0,
+);
