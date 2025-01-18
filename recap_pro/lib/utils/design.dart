f02c0e15ed23aeea8ecf0e67196b57a53602567b
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 14:23:30 +0200

    Edited: Design added btn style

--- recap_pro/lib/utils/design.dart
+++ recap_pro/lib/utils/design.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-const TextStyle title =
-    TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold ,color: Color.fromARGB(255, 0, 0, 0));
+const TextStyle title = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    color: Color.fromARGB(255, 0, 0, 0));
 
 const TextStyle bodyText = TextStyle(
   fontSize: 20.0,
@@ -16,6 +18,13 @@ const TextStyle loginText = TextStyle(
 
 const TextStyle buttonTextStyle = TextStyle(color: Colors.black);
 
+ButtonStyle loginBtn = ElevatedButton.styleFrom(
+  backgroundColor: const Color(0xFF61DBFB),
+  foregroundColor: Colors.black,
+  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+  textStyle: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+);
 const textFieldPadding = EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0);
 
 const ExpandingDotsEffect indicatorEffect = ExpandingDotsEffect(
