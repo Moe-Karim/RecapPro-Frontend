dfd268afd373d3f0a15ca6f95d84eef5a2a3c0ed
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 17:02:59 +0200

    Edited: Recording handled storage permission

--- recap_pro/lib/services/screen_recording.dart
+++ recap_pro/lib/services/screen_recording.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-
-class RecordingService{
-  
+class RecordingService {
+  Future<void> requestPermissions() async {
+    await [
+      Permission.microphone,
+      Permission.storage,
+    ].request();
+  }
 }
