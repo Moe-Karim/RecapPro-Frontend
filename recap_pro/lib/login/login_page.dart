a2b61ddc9f7ec5c54fa366c93fded6839cf582ae
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 13:45:43 +0200

    Edited: Login page added Username title

--- recap_pro/lib/login/login_page.dart
+++ recap_pro/lib/login/login_page.dart
@@ -18,10 +18,22 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: [
             SizedBox(height: 25.0),
-            Image.asset("./assets/splash_Text.png",
-            width: 200.0,
-            height: 200.0,
+            Image.asset(
+              "./assets/splash_Text.png",
+              width: 200.0,
+              height: 200.0,
             ),
+            const SizedBox(height: 20.0),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: const Align(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  "Username",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
           ],
         ),
       ),
