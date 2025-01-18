a0b85fb154f0fa17e6cfc1ad101e696e0d1dcb77
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-18 23:52:23 +0200

    Edited: Video editing checked null

--- recap_pro/lib/services/video_editing.dart
+++ recap_pro/lib/services/video_editing.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class VideoEditingLogic{
-  
+class VideoEditingLogic {
+  Future<void> uploadVideo(File _video) async {
+    if (_video == null) {
+      print("No video found");
+      return;
+    }
+  }
 }
