9926ad7fcd555c32a6b5245b99b2f5c77721c829
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-22 19:42:51 +0200

    Edited: VideoEditing page added token to request

--- recap_pro/lib/services/video_editing.dart
+++ recap_pro/lib/services/video_editing.dart
@@ -4,36 +4,48 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:gal/gal.dart';
 import 'package:http/http.dart' as http;
-import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:recap_pro/services/auth_service.dart';
 
 class VideoEditingLogic {
+  AuthService authService = AuthService();
 
-  Future<Map<String, dynamic>> uploadVideo(File _video , endpoint) async {
-    if (_video == null) {
-      throw Exception("No video found");
-    }
-    final request = http.MultipartRequest(
-      'POST',
-      Uri.parse('http://192.168.1.107:3000/${endpoint}'),
-    );
-    request.files.add(await http.MultipartFile.fromPath("video", _video.path));
-
-    try {
-      final response = await request.send();
-      if (response.statusCode == 200) {
-        final responseData = await response.stream.bytesToString();
-        final data = json.decode(responseData);
-        print(data);
-        return (data);
-      } else {
-        throw Exception('Failed to process video');
-      }
-    } catch (e) {
-      throw Exception('Error uploading video: $e');
-    }
+  
+ Future<Map<String, dynamic>> uploadVideo(File _video, String endpoint) async {
+  if (_video == null) {
+    throw Exception("No video found");
   }
+
+  final token = await authService.getToken();  
+
+  final request = http.MultipartRequest(
+    'POST',
+    Uri.parse('http://192.168.1.107:3000/$endpoint'),
+  );
+
+  
+  request.headers.addAll({
+    'Authorization': 'Bearer $token',  
+    'Content-Type': 'multipart/form-data',
+  });
+
+  request.files.add(await http.MultipartFile.fromPath("video", _video.path));
+
+  try {
+    final response = await request.send();
+    if (response.statusCode == 200) {
+      final responseData = await response.stream.bytesToString();
+      final data = json.decode(responseData);
+      print(data);
+      return data; 
+    } else {
+      throw Exception('Failed to process video');
+    }
+  } catch (e) {
+    throw Exception('Error uploading video: $e');
+  }
+}
 
   Future<String> generateTextFile(String transcript) async {
     final directory = await getApplicationDocumentsDirectory();
